66f1b6e4b4e58268ee3dcf021d3d69f9e93ac2dd
CagesThrottleUs <manstein.felix@gmail.com>
2025-08-23 11:24:22 +0530

    feat(argument): implement Arguments class for cmdline arg handling
    This commit introduces the Arguments class, which manages input files
    and formatting options for the program.
    
    It includes methods to add formatting options, input files, and handle
    stdin. Additionally, a parseArguments function is provided to process
    command-line arguments.
    
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

---
+++ src/argument_parser/argument_parser.cpp
@@ -0,0 +1,94 @@
+#include "argument_parser.hpp"
+
+#include <span>
+#include <string_view>
+
+// PRIVATE INTERFACE
+
+namespace ccwc::argument_parser
+{
+
+    auto Arguments::addFormattingOptions(
+        ccwc::output_format_options::OutputFormatOptions formatOption) -> void
+    {
+        m_output_formatter.addOption(formatOption);
+    }
+
+    auto Arguments::addInputFile(const std::string& filename) -> void
+    {
+        m_input_streams.emplace_back(filename);
+    }
+
+    auto Arguments::addStdin() -> void
+    {
+        if (m_input_streams.empty())
+        {
+            m_input_streams.emplace_back();
+        }
+        else
+        {
+            // NOOP
+        }
+    }
+
+} // namespace ccwc::argument_parser
+
+// PUBLIC EXPOSED INTERFACE
+
+namespace ccwc
+{
+
+    namespace detail
+    {
+
+        auto processOption(std::string_view arg, ccwc::argument_parser::Arguments& args) -> void
+        {
+            if (arg == "-l")
+            {
+                args.addFormattingOptions(
+                    ccwc::output_format_options::OutputFormatOptions::FORMAT_LINES);
+            }
+            else if (arg == "-w")
+            {
+                args.addFormattingOptions(
+                    ccwc::output_format_options::OutputFormatOptions::FORMAT_WORDS);
+            }
+            else if (arg == "-c")
+            {
+                args.addFormattingOptions(
+                    ccwc::output_format_options::OutputFormatOptions::FORMAT_BYTES);
+            }
+            else if (arg == "-m")
+            {
+                args.addFormattingOptions(
+                    ccwc::output_format_options::OutputFormatOptions::FORMAT_MULTIBYTE);
+            }
+        }
+    } // namespace detail
+
+    auto parseArguments(int argc, char** argv) -> ccwc::argument_parser::Arguments
+    {
+        ccwc::argument_parser::Arguments args;
+
+        std::span<char*> safeArgs(argv, static_cast<std::size_t>(argc));
+
+        for (char* arg : safeArgs)
+        {
+            std::string_view argView(arg);
+
+            if (argView.starts_with("-"))
+            {
+                detail::processOption(argView, args);
+            }
+            else
+            {
+                args.addInputFile(std::string(argView));
+            }
+        }
+
+        args.addStdin();
+
+        return args;
+    }
+
+} // namespace ccwc
---
+++ src/argument_parser/argument_parser.hpp
@@ -0,0 +1,70 @@
+#ifndef CCWC_ARGUMENT_PARSER_HPP
+#define CCWC_ARGUMENT_PARSER_HPP
+
+#include "algorithm/universal_input_stream.hpp"
+#include "output_formatter/output_formatter.hpp"
+
+#include <vector>
+
+namespace ccwc::argument_parser
+{
+
+    /**
+     * @brief Arguments class that will be used to store the arguments passed to the program.
+     */
+    class Arguments
+    {
+
+      private:
+        /**
+         * @brief The output formatter to use.
+         */
+        ccwc::output_formatter::OutputFormatter m_output_formatter;
+
+        /**
+         * @brief The collection of input streams
+         */
+        std::vector<ccwc::algorithm::UniversalInputStream> m_input_streams;
+
+      public:
+        /**
+         * @brief Constructor for the Arguments class.
+         */
+        Arguments() = default;
+
+        // copy constructor, move constructor, copy assignment operator, move assignment operator
+        Arguments(const Arguments&)                    = delete;
+        Arguments(Arguments&&)                         = default;
+        auto operator=(const Arguments&) -> Arguments& = delete;
+        auto operator=(Arguments&&) -> Arguments&      = default;
+
+        /**
+         * @brief Destructor for the Arguments class.
+         */
+        ~Arguments() = default;
+
+        /**
+         * @brief add formatting options to the output formatter.
+         */
+        auto addFormattingOptions(ccwc::output_format_options::OutputFormatOptions formatOption)
+            -> void;
+
+        /**
+         * @brief Add an input file to the arguments.
+         */
+        auto addInputFile(const std::string& filename) -> void;
+
+        /**
+         * @brief Add stdin to the arguments.
+         */
+        auto addStdin() -> void;
+    };
+
+} // namespace ccwc::argument_parser
+
+namespace ccwc
+{
+    auto parseArguments(int argc, char** argv) -> ccwc::argument_parser::Arguments;
+} // namespace ccwc
+
+#endif // CCWC_ARGUMENT_PARSER_HPP
